01aa13c57fac7cef537a2d8f37c8808ae06b97d9
Nano-AI <27119566+Nano-AI@users.noreply.github.com>
2023-06-03 10:45:37 -0700

    Added navbar

---
+++ frontend/hackpnw-2023/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import BasicExample from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicExample />
+    </div>
+  );
+}
+
+export default App;
